b5c3ba2cae14974adb59703e930eb97fc0f4280d
Erik Bigler <erikbigler@gmail.com>
2013-05-14 21:46:09 -0700

    updates

--- src/com/gmail/ebiggz/plugins/mythsentials/Listeners/NoFallDamage.java
+++ src/com/gmail/ebiggz/plugins/mythsentials/Listeners/NoFallDamage.java
@@ -8,13 +8,13 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class NoFallDamage implements Listener {
-	
+
 	@EventHandler (priority = EventPriority.HIGH)
-    public void onEntityDamage(EntityDamageEvent event) {
-        if (!(event.getEntity() instanceof Player))
-            return;
-        if (event.getCause() == DamageCause.FALL) {
-                event.setCancelled(true);
-        }
-    }
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player))
+			return;
+		if (event.getCause() == DamageCause.FALL) {
+			event.setCancelled(true);
+		}
+	}
 }
