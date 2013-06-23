8746790df7a86f6a35b506e6e1b5ac99c948ccf8
Erik Bigler <erikbigler@gmail.com>
2013-06-23 01:54:50 -0700

    added guide book

--- src/com/mythicacraft/plugins/mythsentials/Listeners/PlayerListener.java
+++ src/com/mythicacraft/plugins/mythsentials/Listeners/PlayerListener.java
@@ -81,6 +81,7 @@ public class PlayerListener implements Listener {
 		inv.setLeggings(new ItemStack(Material.LEATHER_LEGGINGS, 1));
 		inv.setBoots(new ItemStack(Material.LEATHER_BOOTS, 1));
 		inv.setItemInHand(ruleBook());
+		inv.addItem(guideBook());
 		inv.addItem(new ItemStack(Material.STONE_SWORD, 1));
 		inv.addItem(new ItemStack(Material.STONE_PICKAXE, 1));
 		inv.addItem(new ItemStack(Material.STONE_AXE, 1));
@@ -111,7 +112,18 @@ public class PlayerListener implements Listener {
 		lore.add("Getting started");
 		lore.add("in Mythica");
 		guideMeta.setLore(lore);
-		//addPages
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Table of Contents" + ChatColor.RESET + "\nPage 2: Getting Started\nPage 4: Portals\nPage 5: Grief Protection\nPage 6: Residence/LWC\nPage 7: Live Map\nPage 8: Chat\nPage 9: Economy\nPage 10: Donator & Subscriber\nPage 11: Temp & Creative Worlds\nPage 12: More Info");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE +"Getting Started" + ChatColor.RESET + "\nAll new players must type \"/register [email] [password]\" which will register you with our forums and allow us to automatically promote you in game.");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Beginning Tips" + ChatColor.RESET + "\n\n- Visit the spawn garden\n- Visit the spawn/server mall\n- Vist player towns and cities\n- Use the free build portals on the southern wall of the Spire\n- Use the map");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Portals " + ChatColor.RESET + "\nInstead of teleportation, we use portals. They are located in the Spire.\n\n" + ChatColor.BOLD + "Portals Types:" + ChatColor.RESET + "\n- 4 Free build\n- 2 Temp World\n- Nether/End Portals\n- Creative Portal\n- Player made city and town portals");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Grief Protection" + ChatColor.RESET + "\nWe take grief very seriously and take steps to prevent it, using the plugins Residence and LWC as preemptive measures. We also use LogBlock to reverse any grief that does manage to happen. LWC and Residence are detailed on page 6.");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Grief Protection(cont)" + ChatColor.RESET + ChatColor.BOLD + "\n\nLWC" + ChatColor.RESET + ": Lock individual chests and containers to prevent unauthorized access!" + ChatColor.BOLD + "\n\nResidence" + ChatColor.RESET + ": Claim areas of land to protect your buildings adn everything inside!\n\nTutorials on the site!");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Live Map" + ChatColor.RESET + "\n\nYou may view our live map at mythicacraft.com/map\n\nThe red boxes indicate claimed land, anything else is available for claim!");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Chat" + ChatColor.RESET + "\n\nThere are two main chat channels, local and global." + ChatColor.BOLD + "\n\nLocal (/ch L)" + ChatColor.RESET + ": Only players within 100 blocks can hear you." + ChatColor.BOLD + "\n\nGlobal (/ch G)" + ChatColor.RESET + ": Anyone on the server can hear you.");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Economy" + ChatColor.RESET + "\n\nThere are many ways to earn/spend money:\n\nYou may earn money by selling items at the server mall or by killing mobs in the temporary worlds\n\nYou may spend money by purchasing items from other players.");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE + "Donator/Subscriber" + ChatColor.RESET + "\n\nWe offer perks and abilities to our donors and subscribers as a thank you for supporting the server.\n\nA full list of perks can be viewed at\n www.mythicacraft.com/donate");
+		guideMeta.addPage(ChatColor.BOLD + "Temp Worlds" + ChatColor.RESET + ":\nThere is a temporary nether and overworld portal located in the spire for up-to-date material gathering." + ChatColor.BOLD + "\n\nCreative" + ChatColor.RESET + ":\nWe offer a creative planning world for our donators to hammer out ideas with your friends.");
+		guideMeta.addPage(ChatColor.BOLD + "" + ChatColor.UNDERLINE +"More Info" + ChatColor.RESET + "\n\nMore info and details can be found on our website at\n\n www.mythicacraft.com/tutorials\n\nand\n\nwww.mythicacraft.com/wiki");
 		guide.setItemMeta(guideMeta);
 		return guide;
 	}
